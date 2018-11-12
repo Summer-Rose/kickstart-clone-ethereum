cc204aedeb8353380bcad3a1d16ce4c5e23ef026
Summer Brochtrup <sbrochtrup@gmail.com>
2018-11-12 14:37:00 +0100

    allow manager to create requests and store them

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,9 +1,22 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+  struct Request {
+    string description;
+    uint value;
+    address recipient;
+    bool complete;
+  }
+
+  Request[] public requests;
   address public manager;
   uint public minimumContribution;
   address[] public approvers;
+
+  modifier restricted() {
+    require(msg.sender == manager);
+    _;
+  }
 
   function Campaign(uint minimum) public {
     manager = msg.sender;
@@ -13,5 +26,21 @@ contract Campaign {
   function contribute() public payable {
     require(msg.value > minimumContribution);
     approvers.push(msg.sender);
+  }
+
+  function createRequest(string description, uint value, address recipient)
+    public restricted {
+    //storage keyword - data stored in storage
+    //memory keyworrd - data stored in memory, memory gets dumped everytime function exits
+    Request memory newRequest = Request({
+      description: description,
+      value: value,
+      recipient: recipient,
+      complete: false
+    });
+
+    //alternative struct instantiation -- not recommended:
+    //Request(description, value, recipient, false);
+    requests.push(newRequest);
   }
 }
