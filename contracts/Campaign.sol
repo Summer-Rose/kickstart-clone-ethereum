c929c5117f6b2aee163a7de1f92df789a829a2a5
Summer Brochtrup <sbrochtrup@gmail.com>
2018-11-12 17:10:02 +0100

    create CampaignFactory to deploy and manage all campaigns

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.4.17;
 
+contract CampaignFactory {
+  address[] public deployedCampaigns;
+
+  function createCampaign(uint minimum) {
+    address newCampaign = new Campaign(minimum, msg.sender);
+    deployedCampaigns.push(newCampaign);
+  }
+
+  function getDeployedCampaigns() public view returns(address[]) {
+    return deployedCampaigns;
+  }
+}
+
 contract Campaign {
   struct Request {
     string description;
@@ -24,8 +37,8 @@ contract Campaign {
     _;
   }
 
-  function Campaign(uint minimum) public {
-    manager = msg.sender;
+  function Campaign(uint minimum, address creator) public {
+    manager = creator;
     minimumContribution = minimum;
   }
 
