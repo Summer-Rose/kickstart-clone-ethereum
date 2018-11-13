c2795a0d6817d4d3acf55b3f3a4528b6b47dd9cf
Summer Brochtrup <sbrochtrup@gmail.com>
2018-11-13 19:39:55 +0100

    enable both client and server side web3 instances

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  //Running in the browser with metamask
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  //Running on the server OR on a browser WITHOUT metamask
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/v3/dd28ee51497f4c66a1653b71189273c0'
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
