c14397310397c0e4132be829b7c892b3c1d8f8a4
Summer Brochtrup <sbrochtrup@gmail.com>
2018-11-17 16:53:06 +0100

    display new campaign fields on index page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import factory from '../ethereum/factory';
 import { Card, Button } from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import { Link } from '../routes';
+import Campaign from '../ethereum/campaign';
 
 class CampaignIndex extends Component {
   //NextJs exclusive method -- called when loading code on Next server
@@ -10,20 +11,26 @@ class CampaignIndex extends Component {
   //static (req. by NextJs) allows Next to run the method without having to render the component
   static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
-    return { campaigns };
+    const summaries = await Promise.all(
+      campaigns.map(address => {
+        const campaign = Campaign(address);
+        return campaign.methods.getSummary().call();
+      })
+    );
+    return { campaigns, summaries };
   }
 
   renderCampaigns() {
-    const items = this.props.campaigns.map((address, title) => {
+    const items = this.props.summaries.map((summary, index) => {
+      const address = this.props.campaigns[index];
       return {
-        header: title,
-        description: (
-          <Link route={`/campaigns/${address}`}>
-            <a>View Campaign</a>
+        header: (
+         <Link route={`/campaigns/${address}`}>
+            <a><h3>{summary[0]}</h3></a>
           </Link>
         ),
-
+        description: summary[1], //description
+        meta: 'Goal: ' + summary[2] + ' Eth', //goal
         fluid: true
       };
     });
